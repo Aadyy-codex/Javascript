ef98328e8031992af0c8544bfc28f562fda47c7f
Aadyy-codex <adityasaini2602@gmail.com>
2025-03-01 14:57:39 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,33 @@
+let x = 8;
+
+if (x===1) {
+    console.log("Monday");
+}
+else if (x===2) {
+    console.log("Tuesday");
+    
+}
+else if (x===3) {
+    console.log("Wednesday");
+    
+}
+else if (x===4) {
+    console.log("Thursday");
+}
+else if (x===5){
+    console.log("Friday");
+    
+}
+else if (x===6) {
+    console.log("saturday");
+    
+}
+else if (x===7) {
+    console.log("sunday");
+    
+}
+ else {
+    console.log("only 7 days in week");
+    
+    
+}
