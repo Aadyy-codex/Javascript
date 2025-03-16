02f60eb6488755aec46efed6c094693b46ed5dde
Aadyy-codex <adityasaini2602@gmail.com>
2025-03-16 14:19:23 +0530

    new

--- Script.js
+++ Script.js
@@ -1,33 +1,116 @@
-let x = 8;
+//IF ELSE STATEMENT
 
-if (x===1) {
-    console.log("Monday");
+// let x = 8;
+
+
+// if (x===1) {
+//     console.log("Monday");
+// }
+// else if (x===2) {
+//     console.log("Tuesday");
+// }
+// else if (x===3) {
+//     console.log("Wednesday");
+// }
+// else if (x===4) {
+//     console.log("Thursday");
+// }
+// else if (x===5){
+//     console.log("Friday");
+// }
+// else if (x===6) {
+//     console.log("saturday");
+// }
+// else if (x===7) {
+//     console.log("sunday");
+// }
+//  else {
+//     console.log("only 7 days in week");    
+// }
+
+
+
+//Marks distribution
+
+// let marks = 75; 
+
+// if (marks >= 90) {
+//     console.log(`A`);
+// }
+//  else if (marks >= 80) {
+//     console.log(`B`);
+// }
+//  else if (marks >= 70) {
+//     console.log(`C`);
+// }
+//  else if (marks >= 60) {
+//     console.log(`D`);
+// }
+//  else if (marks >= 50) {
+//     console.log(`E`);
+// }
+//  else if (marks >= 40) {
+//     console.log(`F`);
+// }
+//  else {
+//     console.log(`Fail`);
+// }
+
+
+
+// let x = 10;
+// let y = 20;
+// let z = 25;
+
+// if (x > y && x > z) {
+//     console.log(x);
+// }
+//  else if (y > x && y > z) {
+//     console.log(y);
+// }
+//  else {
+//     console.log(z);
+// }
+
+
+
+
+//VOVELS AND CONSONENTS
+
+// let vovels = "a";
+// if(vovels === 'a' || vovels === 'e' || vovels === 'i' || vovels === 'o' ||vovels === 'u' ){
+//     console.log("vovels");
+// }
+//  else{
+//     console.log("consonants");
+// }
+
+
+//in python [5050505050];
+let a = "50";
+let b = 5;
+
+console.log('a * b =',a*b);
+
+
+let g = 1;
+if(g === 1){
+    console.log("Hello");
 }
-else if (x===2) {
-    console.log("Tuesday");
-    
+ else{
+    console.log("bye");
 }
-else if (x===3) {
-    console.log("Wednesday");
-    
-}
-else if (x===4) {
-    console.log("Thursday");
-}
-else if (x===5){
-    console.log("Friday");
-    
-}
-else if (x===6) {
-    console.log("saturday");
-    
-}
-else if (x===7) {
-    console.log("sunday");
-    
-}
- else {
-    console.log("only 7 days in week");
-    
-    
-}
+
+// Mathematical operator
+
+// let x = 5;
+// let y = 10;
+
+// console.log('x + y =',x+y);
+// console.log('x - y =',x-y);
+// console.log('x * y =',x*y);
+// console.log();
+
+
+
+
